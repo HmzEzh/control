a4f57732d1ee683e3e45b67eda2509c2eb0efd16
hamza ezzahi <hamzaezzahi177@gmail.com>
2022-04-07 00:20:35 +0000

    v2

--- app/Prof.php
+++ app/Prof.php
@@ -9,24 +9,6 @@ use TCG\Voyager\Traits\Translatable;
 class Prof extends Model{
     use Translatable;
     protected $translatable = ['title', 'body'];
-    // Loads all translations
-$posts = Post::with('translations')->get();
-
-// Loads all translations
-$posts = Post::all();
-$posts->load('translations');
-
-// Loads all translations
-$posts = Post::withTranslations()->get();
-
-// Loads specific locales translations
-$posts = Post::withTranslations(['en', 'da'])->get();
-
-// Loads specific locale translations
-$posts = Post::withTranslation('da')->get();
-
-// Loads current locale translations
-$posts = Post::withTranslation('da')->get();
     public function scopeProfs($query)
     {
         if (Auth::user()->role_id == 2)
